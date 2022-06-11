10116e4b3f00887a4a8cd7620942a90eb5d4aac0
Vicharm <vicharm.chang@ailabs.tw>
2022-06-11 18:12:54 +0800

    update readme / change tsx to be ts
    update readme
    
    
    update readme

---
+++ lib/Amplitude.ts
@@ -0,0 +1,69 @@
+import amplitude from "amplitude-js";
+export default class Amplitude {
+  static init() {
+    if (process.env.NEXT_PUBLIC_ANALYTICS_ID) {
+      amplitude.getInstance().init(process.env.NEXT_PUBLIC_ANALYTICS_ID);
+    }
+  }
+
+  static analyticsPageView(page: string, properties?: { [key: string]: any }) {
+    if (process.env.NEXT_PUBLIC_ENV === "development") {
+      // console.log(`pageView ${page}`, properties);
+      return;
+    }
+
+    if (properties == null) {
+      properties = { page: page };
+    } else {
+      properties["page"] = page;
+    }
+
+    amplitude.getInstance().logEvent("PageView", properties);
+
+    //Logger.log(`pageView ${page}`, properties);
+  }
+
+  static analyticsEvent(event: {
+    category: string;
+    action: string;
+    label?: string;
+    value?: number;
+    [key: string]: any;
+  }) {
+    if (process.env.NEXT_PUBLIC_ENV === "development") {
+      console.log(`event: [${event.category}][${event.action}]`, event);
+      return;
+    }
+
+    amplitude
+      .getInstance()
+      .logEvent(`[${event.category}][${event.action}]`, event);
+    //console.log(`event: [${event.category}][${event.action}]`, event);
+  }
+
+  static leavePageEvent(
+    document: Document,
+    id: any,
+    maxScroll: number,
+    type: string
+  ) {
+    const body = document.body;
+    const html = document.documentElement;
+    const pageHeight = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+
+    Amplitude.analyticsEvent({
+      category: `Effectiveness ${type}`,
+      action: `leave ${type}`,
+      id: id,
+      maxScrollPercentage: Math.round(
+        ((maxScroll + html.clientHeight) / pageHeight) * 100
+      ),
+    });
+  }
+}
---
+++ lib/post.ts
@@ -0,0 +1,96 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+import remarkUnwrapImages from "remark-unwrap-images";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeRaw from "rehype-raw";
+import rehypeStringify from "rehype-stringify";
+
+const base = process.cwd();
+
+export function getSortedPostsData(dirName: string) {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(path.join(base, dirName));
+  const allPostsData = fileNames.map((fileName) => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, "");
+
+    // Read markdown file as string
+    const fullPath = path.join(base, dirName, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+
+    // Combine the data with the id
+
+    return {
+      id,
+      date: matterResult.data.date,
+      ...matterResult.data,
+    };
+  });
+  // Sort posts by date
+  return allPostsData.sort(({ date: a }, { date: b }) => {
+    if (a < b) {
+      return 1;
+    } else if (a > b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
+
+export function getAllPostIds(dirName: string) {
+  const fileNames = fs.readdirSync(path.join(base, dirName));
+
+  // Returns an array that looks like this:
+  // [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr'
+  //     }
+  //   },
+  //   {
+  //     params: {
+  //       id: 'pre-rendering'
+  //     }
+  //   }
+  // ]
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostData(id: string, dirName: string) {
+  const fullPath = path.join(base, dirName, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  const processedContent = await unified()
+    .use(remarkUnwrapImages)
+    .use(remarkParse)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeStringify)
+    .process(matterResult.content);
+
+  const contentHtml = processedContent.toString();
+
+  // Combine the data with the id and contentHtml
+  return {
+    id,
+    contentHtml,
+    ...matterResult.data,
+  };
+}
