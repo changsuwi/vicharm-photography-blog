bcd5c0bb994f39fa61da610b692afd4d106a5ece
changsuwi <vic010744@gmail.com>
2024-05-12 12:32:45 +0800

    Fix/accessibility problem (#8)
    * fix: html does not have lang
    
    * fix: add aria-label for image link

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
     domains: ['ik.imagekit.io'],
     deviceSizes: [320, 375, 420, 475, 640, 750, 828, 1080, 1200, 1440, 1920, 2048, 3840],
   },
+  i18n: {
+    locales: ['zh-TW'],
+    defaultLocale: 'zh-TW',
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
