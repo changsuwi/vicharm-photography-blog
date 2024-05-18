0f6f137be4cde2ccd5afe93f91850659c3b54d1a
Vicharm <vicharm.chang@ailabs.tw>
2024-05-18 22:19:52 +0800

    refactor: top landing

--- components/TopLanding.tsx
+++ components/TopLanding.tsx
@@ -1,22 +1,21 @@
 import * as React from "react";
 
-import styles from "../styles/components/TopLanding.module.scss";
 import MyImage from "./common/MyImage";
 
 export default function TopLanding() {
   return (
-    <div className={styles.landing}>
+    <div className="w-full h-[667px] relative overflow-hidden flex flex-col justify-center items-center color-white text-center lg:h-[900px]">
       <MyImage
         src="/九份/50185219987_dd610f128f_o_xEr4kVwGoaE.jpeg"
         alt=""
-        className={styles.background}
+        className="w-full h-full object-cover object-center"
         fill
         loading="eager"
         priority
       />
-      <div className={styles["text-container"]}>
-        <span className={styles.title}>Vicharm 攝影旅行日誌</span>
-        <p className={styles.content}>在旅行途中，成就更美好的意義</p>
+      <div className={"z-10 text-white text-center absolute bottom-12 flex flex-col gap-4"}>
+        <span className={"text-3xl font-bold md:text-4xl"}>Vicharm 攝影旅行日誌</span>
+        <p className={"text-xl md:text-2xl"}>在旅行途中，成就更美好的意義</p>
       </div>
     </div>
   );
